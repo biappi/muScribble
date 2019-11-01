31d2dc27e684d3f52998565476f75de648f35fa9
Antonio Malara <antonio.malara@gmail.com>
2019-11-01 23:09:27 +0100

    correctly unframing sysex messages and parsing the logic control strip

--- src/main.c
+++ src/main.c
@@ -1,44 +1,145 @@
-#include <stdlib.h>
+#include <stdbool.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include "platform.h"
 #include "display.h"
 
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MAX(a,b) (((a)>(b))?(a):(b))
+
+uint8_t logic_control_strip[116] = { 0 };
+
 uint8_t nibble_char(uint8_t x) {
     return (x < 10) ? ('0' + x) : ('a' + (x - 10));
 }
 
-void usb_midi_received_callback(const uint8_t * buf, size_t len) {
-    //                                 1     1
-    //                       01234567890123456
-    static uint8_t string1[] = "* MIDI          ";
-    static uint8_t string2[] = "00 00 00 00 00  ";
+void byte_string(char * string, uint8_t byte) {
+    string[0]  = nibble_char((byte & 0xf0) >> 4);
+    string[1]  = nibble_char((byte & 0x0f)     );
+}
 
-    uint8_t byte;
-    
-    byte = buf[0];
-    string2[0]  = nibble_char((byte & 0xf0) >> 4);
-    string2[1]  = nibble_char((byte & 0x0f)     );
+void send_small_dump(const uint8_t * buf, size_t len) {
+    //                                   1     1
+    //                         01234567890123456
+    static uint8_t string[] = "00 00 00 00 00  ";
 
-    byte = buf[1];
-    string2[3]  = nibble_char((byte & 0xf0) >> 4);
-    string2[4]  = nibble_char((byte & 0x0f)     );
+    for (int i = 0; i < 5; i++) {
+        string[(i * 3)    ] = '-';
+        string[(i * 3) + 1] = '-';
+    }
 
-    byte = buf[2];
-    string2[6] = nibble_char((byte & 0xf0) >> 4);
-    string2[7] = nibble_char((byte & 0x0f)     );
+    for (int i = 0; i < MIN(len, 5); i++) {
+        byte_string(string + (i * 3), buf[i]);
+    }
 
-    byte = buf[3];
-    string2[9]  = nibble_char((byte & 0xf0) >> 4);
-    string2[10] = nibble_char((byte & 0x0f)     );
 
-    byte = buf[4];
-    string2[12] = nibble_char((byte & 0xf0) >> 4);
-    string2[13] = nibble_char((byte & 0x0f)     );
+    display_send_string(string);
+}
+enum usb_midi_packet_types {
+    usb_midi_sysex_start_or_continue = 0x04,
+    usb_midi_sysex_ends_single_byte  = 0x05,
+    usb_midi_sysex_ends_two_bytes    = 0x06,
+    usb_midi_sysex_ends_three_bytes  = 0x07,
+};
 
-    display_send_string(string1);
-    display_send_string(string2);
+
+struct buffer {
+    size_t  len;
+    uint8_t buf[255];
+};
+
+struct sysex_parser {
+    enum {
+        sysex_parser_idle,
+        sysex_parser_accumulating,
+        sysex_parser_ended,
+    } state;
+
+    struct buffer sysex;
+};
+
+void sysex_parse(struct sysex_parser * parser, const uint8_t * packet)
+{
+    const uint8_t
+    cable_id    = (packet[0] & 0xf0) >> 0;
+
+    const enum usb_midi_packet_types
+    packet_type = (packet[0] & 0x0f);
+
+    struct buffer
+    *sysex      = &(parser->sysex);
+
+    switch (packet_type) {
+        case usb_midi_sysex_start_or_continue:
+            switch (parser->state) {
+                case sysex_parser_idle:
+                case sysex_parser_ended:
+                    sysex->len = 0;
+                    break;
+
+                case sysex_parser_accumulating:
+                    break;
+            }
+
+            sysex->buf[sysex->len++] = packet[1];
+            sysex->buf[sysex->len++] = packet[2];
+            sysex->buf[sysex->len++] = packet[3];
+            parser->state = sysex_parser_accumulating;
+            break;
+
+        case usb_midi_sysex_ends_single_byte:
+            sysex->buf[sysex->len++] = packet[1];
+            parser->state = sysex_parser_ended;
+            break;
+
+        case usb_midi_sysex_ends_two_bytes:
+            sysex->buf[sysex->len++] = packet[1];
+            sysex->buf[sysex->len++] = packet[2];
+            parser->state = sysex_parser_ended;
+            break;
+
+        case usb_midi_sysex_ends_three_bytes:
+            sysex->buf[sysex->len++] = packet[1];
+            sysex->buf[sysex->len++] = packet[2];
+            sysex->buf[sysex->len++] = packet[3];
+            parser->state = sysex_parser_ended;
+            break;
+    }
+}
+
+void usb_midi_received_callback(const uint8_t * buf, size_t len)
+{
+    static struct sysex_parser parser = { 0 };
+
+    for (int i = 0; i < len / 4; i++) {
+        sysex_parse(&parser, buf + (i * 4));
+
+        if (parser.state == sysex_parser_ended) {
+            const uint8_t * midiData = parser.sysex.buf;
+
+            parser.state = sysex_parser_idle;
+
+            if (
+                (midiData[0] == 0xf0) &&
+                (midiData[5] == 0x12)
+            ) {
+                const uint8_t  offset =  midiData[6];
+                const uint8_t *src    = &midiData[7];
+                      uint8_t *dst    = (logic_control_strip + offset);
+
+                while (*src != 0xf7) {
+                    *dst++ = *src++;
+                }
+
+                display_goto_line(0);
+                display_send_string(logic_control_strip);
+                display_send_string("<<<");
+            }
+        }
+
+    }
 }
 
 // - //
@@ -46,29 +147,10 @@ void usb_midi_received_callback(const uint8_t * buf, size_t len) {
 
 int main(void)
 {
-
-    int delay_counter = 0;
-
     platform_init();
-
-    display_send_string("* START         ");
 
     while (1) {
         platform_poll();
-        delay_counter++;
-
-        if (delay_counter > 1000000) {
-            static int i = 1;
-            if (i == 7) {
-                i = 0;
-                display_send_empty_screen(); 
-            }
-
-            i++;
-
-            display_send_string("* ALIVE         ");
-            delay_counter = 0;
-        }
     }
 }
 
